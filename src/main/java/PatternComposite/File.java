14c6d82a7899fceedb9216b8e8a3799ebcad71ed
omar_saidi <omar_saidi@carrefour.com>
2022-05-15 18:59:11 +0200

    Composite Design Pattern

---
+++ src/main/java/PatternComposite/File.java
@@ -0,0 +1,14 @@
+package PatternComposite;
+
+public class File extends Component {
+
+    public File(String name) {
+        super(name);
+    }
+
+    @Override
+    public void view(){
+        String tab = tabulationLevel();
+        System.out.println( tab + "File : " + name );
+   }
+}
